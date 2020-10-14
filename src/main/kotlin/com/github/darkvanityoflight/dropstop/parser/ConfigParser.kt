f7047d610c3614d260d5b064c9069628dc093bd8
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-10-14 17:06:59 +0200

    feat: Create config parser

---
+++ src/main/kotlin/com/github/darkvanityoflight/dropstop/parser/ConfigParser.kt
@@ -0,0 +1,31 @@
+package com.github.darkvanityoflight.dropstop.parser
+
+import com.github.darkvanityoflight.darkmodcore.configparser.ADarkModConfigParser
+import org.bukkit.Material
+import org.bukkit.World
+import org.bukkit.configuration.file.FileConfiguration
+
+class ConfigParser(config: FileConfiguration) : ADarkModConfigParser(config) {
+    var bannedBlockMap: Map<World.Environment, Set<Material>> = emptyMap()
+
+    override fun read() {
+
+        // Parse all banned materials
+        val bannedMaterialMap : MutableMap<World.Environment, Set<Material>> = mutableMapOf()
+        val bannedMaterial: Set<Material> = emptySet()
+        for (key in config.getKeys(false)) {
+            if (key in World.Environment.values()) {
+
+                val bannedBlocksString = config.getStringList(key)
+                if (bannedBlocksString.isNotEmpty()) {
+                    bannedBlocksString.forEach { it ->
+                        if (Material.getMaterial(it) != null) {
+                            bannedMaterial.plus(Material.getMaterial(it))
+                        }
+                    }
+                }
+                bannedMaterialMap[World.Environment.valueOf(key)] = bannedMaterial
+            }
+        }
+    }
+}
