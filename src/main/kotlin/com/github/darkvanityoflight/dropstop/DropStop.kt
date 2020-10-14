f39a26d31a984eeb63a0da47f5f5b1b39c3b00cb
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-10-14 15:46:38 +0200

    chore: Base main class

---
+++ src/main/kotlin/com/github/darkvanityoflight/dropstop/DropStop.kt
@@ -0,0 +1,15 @@
+package com.github.darkvanityoflight.dropstop
+
+import com.github.darkvanityoflight.darkmodcore.ADarkMod
+
+
+class DropStop : ADarkMod() {
+
+
+    override fun onEnable() {
+        super.onEnable()
+        info("[DropStop] Enabling DropStop")
+    }
+
+
+}
