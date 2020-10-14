319a956e0fc72982dbef0ac16547003511fdd845
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-10-14 17:07:46 +0200

    feat: Parse config and register events

--- src/main/kotlin/com/github/darkvanityoflight/dropstop/DropStop.kt
+++ src/main/kotlin/com/github/darkvanityoflight/dropstop/DropStop.kt
@@ -1,15 +1,22 @@
 package com.github.darkvanityoflight.dropstop
 
 import com.github.darkvanityoflight.darkmodcore.ADarkMod
+import com.github.darkvanityoflight.dropstop.listeners.BreakListener
+import com.github.darkvanityoflight.dropstop.parser.ConfigParser
+import org.bukkit.Bukkit
 
 
 class DropStop : ADarkMod() {
+    private val configParser : ConfigParser = ConfigParser(config)
 
+    init {
+        configParser.read()
+    }
 
     override fun onEnable() {
         super.onEnable()
         info("[DropStop] Enabling DropStop")
+
+        Bukkit.getPluginManager().registerEvents(BreakListener(configParser.bannedBlockMap), this)
     }
-
-
 }
